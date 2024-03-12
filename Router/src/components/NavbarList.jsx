57943d885d7ec9209e62c861b8eb0478d6b874c3
mithcodes <singhmithlesh791@gmail.com>
2024-03-12 11:38:14 +0530

    first commit

--- Router/src/components/NavbarList.jsx
+++ Router/src/components/NavbarList.jsx
@@ -3,6 +3,7 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import "../App.css";
 import { AppState } from "../context/Context";
+import { Link } from "react-router-dom";
 
 const NavbarList = ({ cart }) => {
   const { state } = AppState();
@@ -17,9 +18,15 @@ const NavbarList = ({ cart }) => {
       <Navbar bg="dark" data-bs-theme="dark">
         <Container>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#store">Store</Nav.Link>
-            <Nav.Link href="#about">About</Nav.Link>
+          <Link className="link lm" to={"/"}>
+              HOME
+            </Link>
+            <Link className="link" to={"/store"}>
+              STORE
+            </Link>
+            <Link className="link" to={"/about"}>
+              ABOUT
+            </Link>
           </Nav>
         </Container>
         <button
